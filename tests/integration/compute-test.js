7082a504fec0066f30effa967cfa435a62850c5e
Kelly Selden <kellyselden@gmail.com>
2017-01-21 18:40:42 -0800

    add integration tests

---
+++ tests/integration/compute-test.js
@@ -0,0 +1,92 @@
+import { readOnly } from 'ember-computed';
+import RSVP from 'rsvp';
+import compute from 'ember-macro-test-helpers/compute';
+import { module, test } from 'qunit';
+
+const { resolve } = RSVP;
+
+module('Integration | compute');
+
+test('it works without properties', function(assert) {
+  compute({
+    assert,
+    computed: readOnly('key'),
+    strictEqual: undefined
+  });
+});
+
+test('it uses properties to calculate value', function(assert) {
+  compute({
+    assert,
+    computed: readOnly('key'),
+    properties: {
+      key: 'test value'
+    },
+    strictEqual: 'test value'
+  });
+});
+
+test('it will deep equal', function(assert) {
+  compute({
+    assert,
+    computed: readOnly('key'),
+    properties: {
+      key: ['test value']
+    },
+    deepEqual: ['test value']
+  });
+});
+
+test('it will allow you to calculate the assertion', function(assert) {
+  compute({
+    assert,
+    computed: readOnly('key'),
+    properties: {
+      key: ['test value']
+    },
+    assertion([value]) {
+      return value === 'test value';
+    }
+  });
+});
+
+test('it can assert readOnly', function(assert) {
+  compute({
+    assert,
+    computed: readOnly('key'),
+    assertReadOnly: true
+  });
+});
+
+test('it is promise-aware', function(assert) {
+  return compute({
+    assert,
+    computed: readOnly('key'),
+    properties: {
+      key: resolve('test value')
+    },
+    strictEqual: 'test value'
+  }).promise;
+});
+
+test('it returns result', function(assert) {
+  let { result } = compute({
+    computed: readOnly('key'),
+    properties: {
+      key: 'test value'
+    }
+  });
+
+  assert.strictEqual(result, 'test value');
+});
+
+test('it returns subject', function(assert) {
+  let { subject } = compute({
+    computed: readOnly('key'),
+    properties: {
+      key: 'test value'
+    }
+  });
+
+  assert.strictEqual(subject.get('computed'), 'test value');
+});
