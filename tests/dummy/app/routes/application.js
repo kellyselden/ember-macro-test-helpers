108188e230d80fac3cc2b5229935fac34f7dccde
Kelly Selden <kellyselden@gmail.com>
2017-10-29 11:02:18 -0700

    update ember-cli 2.16.2

--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -1,6 +1,6 @@
-import Ember from 'ember';
+import Route from '@ember/routing/route';
 import compute from 'ember-macro-test-helpers/compute';
 
-export default Ember.Route.extend({
+export default Route.extend({
   compute
 });
